fbfe733ea4c1d1f8337d6bd26418fb5d995bc8eb
Akanksha Singh <ak302575@dal.ca>
2022-05-03 17:27:59 -0300

    Updates to the project

--- fleamart-subscription/subscription_service/src/session.py
+++ fleamart-subscription/subscription_service/src/session.py
@@ -4,8 +4,8 @@ import boto3
 #Creating Session With Boto3.
 def get_session():
     session = boto3.Session(
-    aws_access_key_id='AKIA3EOK4A4FHRPDX5OJ',
-    aws_secret_access_key='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',
+    aws_access_key_id='xxx',
+    aws_secret_access_key='xxx',
     region_name="us-east-1"
     )
     return session
